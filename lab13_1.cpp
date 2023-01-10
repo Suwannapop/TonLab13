115fe00bbb25ffa98e7001a4d026ed66a837f645
Suppakron <suwannapop01@gmail.com>
2023-01-10 15:31:14 +0700

    oky1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci (int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+int fibonacci (int x){
+    if (x == 0)
+    {
+        return 0 ;
+    }
+    if (x == 1)
+    {
+        return 1 ;
+    }
+    if (x > 1)
+    {
+        int sum = (int)fibonacci(x-1) + fibonacci(x-2) ;
+        return sum ;
+    }
+    
 }
